9962867786a781ffc8bfab0fcb1f753057dcab63
mikeliwei <3117351487@qq.com>
2026-08-11 22:59:11 +0800

    新增菜品

--- sky-server/src/main/java/com/sky/controller/admin/commonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/commonController.java
@@ -1,9 +1,38 @@
 package com.sky.controller.admin;
 
+import com.sky.annocation.AutoFill;
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
 @RestController
 @RequestMapping("/admin/common")
 public class commonController {
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file) {
+        log.info("上传文件");
+        try {
+            String originalFilename = file.getOriginalFilename();
+            String substring = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String fileName = UUID.randomUUID() + substring;
+            String url = aliOssUtil.upload(file.getBytes(), fileName);
+            return Result.success(url);
+        } catch (IOException e) {
+            log.info("上传文件失败", e);
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
 }
